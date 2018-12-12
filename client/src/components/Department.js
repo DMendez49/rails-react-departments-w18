5ae2f4ddb7bdc21e1d77b58218484062cdaf34d9
Spencer Richards <spencer.richards7@gmail.com>
2018-12-12 12:34:20 -0700

    display items

--- client/src/components/Department.js
+++ client/src/components/Department.js
@@ -1,14 +1,17 @@
 import React from "react";
 import axios from "axios";
-import { Card, Icon, Button, } from "semantic-ui-react";
+import { Link, } from "react-router-dom";
+import { Card, Icon, Button, Image, } from "semantic-ui-react";
 
 class Department extends React.Component {
-  state = { dep: {}, };
+  state = { dep: {}, items: [], };
 
   componentDidMount() {
     const { id, } = this.props.match.params;
     axios.get(`/api/departments/${id}`)
       .then( res => this.setState({ dep: res.data, }))
+    axios.get(`/api/departments/${id}/items`)
+      .then( res => this.setState({ items: res.data, }))
   }
 
   handleDelete = (id) => {
@@ -16,6 +19,31 @@ class Department extends React.Component {
     if (remove)
       axios.delete(`/api/departments/${id}`)
         .then( res => this.props.history.push("/departments"))
+  }
+
+  renderItems = () => {
+    return this.state.items.map( i => (
+      <Card>
+        <Image src={i.image_url} />
+        <Card.Content>
+          <Card.Header>{ i.name }</Card.Header>
+          <br />
+          <Card.Content extra>${ i.price }</Card.Content>
+          <br />
+          <Card.Description>{ i.description }</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui two buttons">
+            <Button inverted color="blue">
+              Edit
+            </Button>
+            <Button inverted color="red">
+              Delete
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    ))
   }
 
   render() {
@@ -38,6 +66,19 @@ class Department extends React.Component {
         </div>
         <br />
         <h4>{ description }</h4>
+        <br />
+        <br />
+        <Link to={`/departments/${id}/items/new`}>
+          <Button icon color="green">
+            <Icon name="add" />
+            Add Item
+          </Button>
+        </Link>
+        <br />
+        <br />
+        <Card.Group>
+          { this.renderItems() }
+        </Card.Group>
       </div>
     )
   }
